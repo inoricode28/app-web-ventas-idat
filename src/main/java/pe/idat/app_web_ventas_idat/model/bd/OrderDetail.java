fa9da4648d08c5e7a97ff1d5cb830acb3d82c7fa
inoricode28 <inori237@gmail.com>
2025-05-28 00:32:33 -0500

    ServiceDetallUsuario

--- src/main/java/pe/idat/app_web_ventas_idat/model/bd/OrderDetail.java
+++ src/main/java/pe/idat/app_web_ventas_idat/model/bd/OrderDetail.java
@@ -1,4 +1,22 @@
 package pe.idat.app_web_ventas_idat.model.bd;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+import pe.idat.app_web_ventas_idat.model.bd.pk.OrderDetailId;
+
+@Data
+@Entity
+@Table(name = "orderdetails")
 public class OrderDetail {
+    @EmbeddedId
+    private OrderDetailId orderDetailId;
+    @Column(name = "unitprice")
+    private Double unitprice;
+    @Column(name = "quantity")
+    private Integer quantity;
+    @Column(name = "discount")
+    private Integer discount;
 }
---
+++ src/main/java/pe/idat/app_web_ventas_idat/service/UsuarioService.java
@@ -0,0 +1,40 @@
+package pe.idat.app_web_ventas_idat.service;
+
+
+
+import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import pe.idat.app_web_ventas_idat.model.bd.Rol;
+import pe.idat.app_web_ventas_idat.model.bd.Usuario;
+import pe.idat.app_web_ventas_idat.repository.RolRepository;
+import pe.idat.app_web_ventas_idat.repository.UsuarioRepository;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+@Service
+@AllArgsConstructor
+public class UsuarioService {
+    private UsuarioRepository usuarioRepository;
+    private RolRepository rolRepository;
+    private BCryptPasswordEncoder bCryptPasswordEncoder =
+            new BCryptPasswordEncoder();
+
+    public Usuario buscarUsuarioPorEmail(String email){
+        return usuarioRepository.findByEmail(email);
+    }
+
+    public Usuario buscarUsuarioPorNombreUsuario(String username){
+        return usuarioRepository.findByNomusuario(username);
+    }
+
+    public Usuario guardarUsuario(Usuario usuario){
+        usuario.setPassword(bCryptPasswordEncoder.encode(usuario.getPassword()));
+        usuario.setActivo(true);
+        Rol usuarioRol = rolRepository.findByNomrol("ADMIN");
+        usuario.setRoles( new HashSet<>(Arrays.asList(usuarioRol)));
+        return usuarioRepository.save(usuario);
+
+        }
+}
