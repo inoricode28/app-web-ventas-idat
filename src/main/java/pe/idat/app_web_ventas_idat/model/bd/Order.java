027bfd4d54485a20da5e78693a40e52ee921d28b
inoricode28 <inori237@gmail.com>
2025-05-29 00:09:31 -0500

    LoginV1

--- src/main/java/pe/idat/app_web_ventas_idat/model/bd/Order.java
+++ src/main/java/pe/idat/app_web_ventas_idat/model/bd/Order.java
@@ -1,26 +1,5 @@
 package pe.idat.app_web_ventas_idat.model.bd;
 
-
-import jakarta.persistence.*;
-import lombok.Data;
-import java.util.Date;
-
-@Data
-@Entity
-@Table(name = "orders")
 public class Order {
-    @Id
-    private Integer orderid;
-    @Column(name = "orderdate")
-    private Date orderdate;
-    @Column(name = "requireddate")
-    private Date requireddate;
-    @Column(name = "shippeddate")
-    private Date shippeddate;
-    @ManyToOne
-    @JoinColumn(name = "customerid")
-    private Customer customer;
-    @ManyToOne
-    @JoinColumn(name = "employeeid")
-    private Employee employee;
+
 }
